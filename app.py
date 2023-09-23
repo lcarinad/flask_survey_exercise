8ec1d01d9e9a3067d1653207f2a8e9c5f3e6e364
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-09-22 22:13:50 -0400

    answer route completed

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "shh-its-a-secret"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
 
 debug = DebugToolbarExtension(app)
 
-response = []
+responses = []
+
+
 
 
 @app.route('/')
 def home():
+
     survey_title = satisfaction_survey.title
     survey_instructions = satisfaction_survey.instructions
     
@@ -21,16 +24,23 @@ def home():
 
 @app.route('/<survey_questions>/<int:idx>')
 def display_questions(survey_questions, idx):
+    try:
+        survey_questions=satisfaction_survey.questions[idx].question
+        survey_choices=satisfaction_survey.questions[idx].choices
+        return render_template("questions.html",survey_questions=survey_questions, survey_choices=survey_choices)
+    except IndexError:
+        return redirect(f"/")
     
-    survey_questions=satisfaction_survey.questions[idx].question
-    survey_choices=satisfaction_survey.questions[idx].choices
-
-        
-    return render_template("questions.html", survey_questions=survey_questions, survey_choices=survey_choices)
 
 @app.route('/answer', methods=["POST"])
 def handle_answer():
 
-    usr_response = request.form['response']
-    response.append(usr_response)
-    return "<h1>Thank you for answerin</h1>"
+    survey_questions=satisfaction_survey.questions
+    usr_responses = request.form['response']
+    responses.append(usr_responses)
+    next_question_idx=len(responses)   
+
+    if len(responses) == len(satisfaction_survey.questions):
+        return render_template("confirmation.html")
+    elif len(responses) < len(satisfaction_survey.questions):
+        return redirect(f"/questions/{next_question_idx}")
